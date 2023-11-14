6ac9d85fddba7081fa5a94e00775305d0c31259c
ciper <anmolkarki101@gmail.com>
2023-11-14 10:43:15 +0545

    Implement CreateMovie controller function

--- pkg/controllers/controllers.go
+++ pkg/controllers/controllers.go
@@ -1,1 +1,19 @@
 package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/KarkiAnmol/MovieHub-API.git/pkg/models"
+	"github.com/KarkiAnmol/MovieHub-API.git/pkg/utils"
+)
+
+func CreateMovie(w http.ResponseWriter, r *http.Request) {
+	createMovie := &models.Movie{}
+	utils.ParseBody(r, createMovie)
+	b := createMovie.CreateMovie()
+	res, _ := json.Marshal(b) //Marshals the created movie (b) into JSON format. This prepares the movie data to be included in the HTTP response.
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
