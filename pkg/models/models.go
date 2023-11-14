64c105e96c11f14a64796d21031a9f0776bdb0ec
ciper <anmolkarki101@gmail.com>
2023-11-14 09:21:44 +0545

    Initial commit for models.go
    - Define Movie struct for database representation.
    - Initialize database connection and perform auto-migration for the Movie model.

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,1 +1,20 @@
 package models
+
+import (
+	"github.com/KarkiAnmol/MovieHub-API.git/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Movie struct {
+	gorm.Model
+	Name string `gorm:""json:"name"`
+}
+
+//initialize connection to database and auto-migration for the Movie model
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Movie{})
+}
