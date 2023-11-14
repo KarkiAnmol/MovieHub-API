be39bc1805f7df8dc93f0776e4d4a42d36302051
ciper <anmolkarki101@gmail.com>
2023-11-14 08:52:50 +0545

    Add MovieHub API routes in routes package
    - Implemented routes for managing movies in the MovieHub API.
    - Added endpoints for retrieving all movies, creating a new movie, retrieving a single movie by ID, updating a movie, and deleting a movie.
    - Routes are connected to their corresponding controller functions in the controllers package.

--- pkg/routes/moviehub-routes.go
+++ pkg/routes/moviehub-routes.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	"github.com/KarkiAnmol/MovieHub-API/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+var RegisterMovieHubRoutes = func(router *mux.Router) {
+	router.HandleFunc("/movie/", controllers.GetMovie).Methods("GET")           //get all movies
+	router.HandleFunc("/movie/", controllers.CreateMovie).Methods("POST")       //create a new movie
+	router.HandleFunc("/movie/{id}", controllers.GetMovieByID).Methods("GET")   //get one movie
+	router.HandleFunc("/movie/{id}", controllers.UpdateMovie).Methods("PUT")    // update movie
+	router.HandleFunc("/movie/{id}", controllers.DeleteMovie).Methods("DELETE") // Delete movie
+
+}
