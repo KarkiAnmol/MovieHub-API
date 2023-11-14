33c7d63ad5dcd559e607b3b68839992220635914
ciper <anmolkarki101@gmail.com>
2023-11-14 09:37:19 +0545

    Implement CRUD operations for Movie model in models.go
    - Initialize the database connection and perform auto-migration for the Movie model in the init function.
    - Implement CreateMovie method to add a new movie record to the database.
    - Implement GetMovie function to retrieve all movies from the database.
    - Implement GetMovieById function to retrieve a movie by its ID from the database.
    - Implement DeleteMovie function to delete a movie by its ID from the database.
    
    This commit adds basic CRUD operations for the Movie model.

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -18,3 +18,25 @@ func init() {
 	db = config.GetDB()
 	db.AutoMigrate(&Movie{})
 }
+func GetMovie() []Movie {
+	var ListOfMovies []Movie
+	db.Find(&ListOfMovies)
+	return ListOfMovies
+}
+func GetMovieById(Id int64) (*Movie, *gorm.DB) {
+	var MovieByID Movie
+	db := db.Where("ID=?", Id).Find(&MovieByID)
+	return &MovieByID, db
+}
+func (m *Movie) CreateMovie() *Movie {
+	db.NewRecord(m)
+	db.Create(&m)
+	return m
+}
+
+//the returned Movie struct will be parsed into JSON to be displayed to the user in Postman
+func DeleteMovie(ID int64) Movie {
+	var movie Movie
+	db.Where("ID=?", ID).Delete(movie)
+	return movie
+}
